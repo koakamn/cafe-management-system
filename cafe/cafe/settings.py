f002f481faf5f080f940b96b691067f7beaf58cb
koakamn <talgatsamir@gmail.com>
2026-05-10 23:30:33 +0500

    auth logic was added

--- cafe/cafe/settings.py
+++ cafe/cafe/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'core',
     'menu',
     'orders',
+    'users',
 
 ]
 
@@ -129,4 +130,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT= BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
--- cafe/cafe/urls.py
+++ cafe/cafe/urls.py
@@ -18,12 +18,15 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
     path('menu/',include('menu.urls')),
     path('cart/',include('orders.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('users/',include('users.urls')),
 ]
 
 if settings.DEBUG:
