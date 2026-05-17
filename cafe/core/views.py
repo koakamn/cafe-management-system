bf18e6b5676db14dd8225d15b31553f24e767d5a
koakamn <talgatsamir@gmail.com>
2026-05-17 13:21:13 +0500

    featured products

--- cafe/core/views.py
+++ cafe/core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from menu.models import Product
 
 def index(request):
-    return render(request, 'core/index.html')
+    featured_products = Product.objects.all()[:3]
+    context = {'featured_products': featured_products}
+    return render(request, 'core/index.html',context)
 
 def about(request):
     return render(request, 'core/about.html')
