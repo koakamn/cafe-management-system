ab0c086aa5be167d56209ef216d9e615ce4386c1
koakamn <talgatsamir@gmail.com>
2026-05-27 17:20:31 +0500

    images

--- cafe/cafe/urls.py
+++ cafe/cafe/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('users/',include('users.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
