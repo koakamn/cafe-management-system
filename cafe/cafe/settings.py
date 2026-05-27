4d6a8574c6acb6ace644e2aaaa5a3c1c9d68c0b4
koakamn <talgatsamir@gmail.com>
2026-05-27 16:42:05 +0500

    Environment variables

--- cafe/cafe/settings.py
+++ cafe/cafe/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3woz*z$87f7#(_rq=k)%$%)@qyk!ad+mo@pam+ous^1!y%f(zi'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-import os
+DEBUG = config('DEBUG',  cast=bool)
 
 
 ALLOWED_HOSTS = ['*']
